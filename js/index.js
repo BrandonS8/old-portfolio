9379f151dfbd681dca2e290968cab5c9e221f7ed
Brandon Sears <brandonsears8@gmail.com>
2018-02-26 12:07:20 -0500

    remade from scratch

--- js/index.js
+++ js/index.js
@@ -1,5 +1,76 @@
-$('.about').on('click', function() {
-     $(this).siblings('.desc').toggleClass('showdesc');
-});
+const firstHeading = document.querySelector('.first h1')
+const first = document.querySelector('.first')
+const second = document.querySelector('.second')
+const third = document.querySelector('.third')
 
-//jQuery because .siblings
+document.querySelector('#homeButton').addEventListener('click', showFirst)
+document.querySelector('#portfolioButton').addEventListener('click', showSecond)
+document.querySelector('#resumeButton').addEventListener('click', showThird)
+showFirst()
+
+function typeFirstHeading () {
+  let words = "Hello, I'm Brandon"
+  let letters = words.split('')
+  let display = []
+  let i = 0
+  let type = setInterval(function () {
+    display.push(letters[i])
+    i++
+    firstHeading.innerHTML = display.join('')
+    if (i > letters.length - 1) {
+      setTimeout(displayStatement, 800)
+      clearInterval(type)
+    }
+  }, 150)
+}
+
+function typeFirstParagraph () {
+  let statement = document.querySelector('.statement')
+  let words = statement.innerHTML
+  statement.innerHTML = ''
+  let letters = words.split(' ')
+  let display = []
+  let i = 0
+  let type = setInterval(function () {
+    display.push(letters[i])
+    i++
+    statement.innerHTML = display.join(' ')
+    if (i > letters.length - 1) {
+      clearInterval(type)
+    }
+  }, 100)
+}
+
+function displayStatement () {
+  let p = document.querySelector('.statement')
+  p.style.display = 'inline'
+  p.classList.add('show')
+  typeFirstParagraph()
+}
+
+setHiddens()
+function setHiddens () {
+  let a = document.querySelectorAll('.hidden')
+  for (i = 0; i < a.length; i++) {
+    a[i].addEventListener('click', function () {
+      this.classList.toggle('show')
+    })
+  }
+}
+
+function showFirst () {
+  typeFirstHeading()
+  first.style.display = 'block'
+  second.style.display = 'none'
+  third.style.display = 'none'
+}
+function showSecond () {
+  first.style.display = 'none'
+  second.style.display = 'flex'
+  third.style.display = 'none'
+}
+function showThird () {
+  first.style.display = 'none'
+  second.style.display = 'none'
+  third.style.display = 'flex'
+}
